03d90eb3c6efade1076deec98ad95634d0955c7f
Larkailya <Larkailya@gmail.com>
2018-12-09 11:54:10 +0200

    [laba3] refactors others

--- usermanagment/src/main/java/ua/nure/kn/larka/usermanagment/database/UserDao.java
+++ usermanagment/src/main/java/ua/nure/kn/larka/usermanagment/database/UserDao.java
@@ -6,9 +6,9 @@ import main.java.ua.nure.kn.larka.usermanagment.*;
 public interface UserDao {
 	User create(User user) throws DatabaseCustomException;
 	
-	void update(User user) throws DatabaseCustomException;
+	User update(User user) throws DatabaseCustomException;
 	
-	void delete(User user) throws DatabaseCustomException;
+	User delete(User user) throws DatabaseCustomException;
 	
 	User find(Long id) throws DatabaseCustomException;
 	
